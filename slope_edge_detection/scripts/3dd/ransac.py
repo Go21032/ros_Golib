71120584d2befd6fcfe3f62b3e545f00d0b9e749
Go21032 <a221032@g.kogakuin.jp>
2024-09-30 23:33:41 +0900

    ransac.pyは処理をcallbackではない関数でやることで処理が早くなった

--- slope_edge_detection/scripts/3dd/ransac.py
+++ slope_edge_detection/scripts/3dd/ransac.py
@@ -5,227 +5,105 @@ import numpy as np
 import open3d as o3d
 import cv2
 
-# グローバル変数の定義
-color_image = None
-depth_image = None
-intrinsics = None
+class PointCloudProcessor:
+    def __init__(self):
+        self.color_image = None
+        self.depth_image = None
+        self.intrinsics = None
+        self.bridge = CvBridge()
 
-# コールバック関数
-def color_callback(msg):
-    global color_image
-    color_image = bridge.imgmsg_to_cv2(msg, "bgr8")
+        rospy.init_node('pointcloud_node')
 
-def depth_callback(msg):
-    global depth_image
-    depth_image = bridge.imgmsg_to_cv2(msg, "16UC1")
+        rospy.Subscriber('/camera/color/image_raw', Image, self.callback, callback_args='color')
+        rospy.Subscriber('/camera/aligned_depth_to_color/image_raw', Image, self.callback, callback_args='depth')
+        rospy.Subscriber('/camera/aligned_depth_to_color/camera_info', CameraInfo, self.callback, callback_args='info')
 
-def camera_info_callback(msg):
-    global intrinsics
-    intrinsics = msg
+        self.vis = o3d.visualization.Visualizer()
+        self.vis.create_window('PCD', width=640, height=480)
+        self.pointcloud = o3d.geometry.PointCloud()
+        self.geom_added = False
+        self.rate = rospy.Rate(10)
 
-# ROSノードの初期化
-rospy.init_node('pointcloud_node')
-bridge = CvBridge()
+    def callback(self, msg, msg_type):
+        if msg_type == 'color':
+            self.color_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        elif msg_type == 'depth':
+            self.depth_image = self.bridge.imgmsg_to_cv2(msg, "16UC1")
+        elif msg_type == 'info':
+            self.intrinsics = msg
 
-# トピックの購読
-rospy.Subscriber('/camera/color/image_raw', Image, color_callback)
-rospy.Subscriber('/camera/aligned_depth_to_color/image_raw', Image, depth_callback)
-rospy.Subscriber('/camera/aligned_depth_to_color/camera_info', CameraInfo, camera_info_callback)
+    def process_pointcloud(self):
+        try:
+            while not rospy.is_shutdown():
+                if self.color_image is None or self.depth_image is None or self.intrinsics is None:
+                    rospy.loginfo("Waiting for images and camera info...")
+                    rospy.sleep(0.1)
+                    continue
 
-# Open3Dの設定
-vis = o3d.visualization.Visualizer()
-vis.create_window('PCD', width=640, height=480)
-pointcloud = o3d.geometry.PointCloud()
-geom_added = False
+                o3d_color_image = cv2.cvtColor(self.color_image, cv2.COLOR_BGR2RGB)
+                img_depth = o3d.geometry.Image(self.depth_image)
+                img_color = o3d.geometry.Image(o3d_color_image)
+                rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+                    img_color, img_depth, convert_rgb_to_intensity=False)
 
-# ループ内の処理を制限
-rate = rospy.Rate(10)  # 10Hzに設定
+                pinhole_camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(
+                    self.intrinsics.width, self.intrinsics.height,
+                    self.intrinsics.K[0], self.intrinsics.K[4],
+                    self.intrinsics.K[2], self.intrinsics.K[5])
 
-try:
-    while not rospy.is_shutdown():
-        if color_image is None or depth_image is None or intrinsics is None:
-            rospy.loginfo("Waiting for images and camera info...")
-            rospy.sleep(0.1)
-            continue
+                pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, pinhole_camera_intrinsic)
+                pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
 
-        # 画像変換を事前に行う
-        o3d_color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
-        img_depth = o3d.geometry.Image(depth_image)
-        img_color = o3d.geometry.Image(o3d_color_image)
-        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(img_color, img_depth, convert_rgb_to_intensity=False)
+                points = np.asarray(pcd.points)
+                pass_x = (points[:, 0] > -1.0) & (points[:, 0] < 1.0)
+                pass_y = (points[:, 1] > -1.0) & (points[:, 1] < 1.0)
+                filtered_indices = np.where(pass_x & pass_y)[0]
 
-        pinhole_camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(
-            intrinsics.width, intrinsics.height, intrinsics.K[0], intrinsics.K[4], intrinsics.K[2], intrinsics.K[5])
+                filtered_pcd = pcd.select_by_index(filtered_indices)
+                filtered_pcd = filtered_pcd.voxel_down_sample(voxel_size=0.005)
 
-        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, pinhole_camera_intrinsic)
-        pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+                plane_model, inliers = filtered_pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=100)
+                inlier_cloud = filtered_pcd.select_by_index(inliers)
 
-        # フィルタリングのインデックスを毎回計算
-        points = np.asarray(pcd.points)
-        pass_x = (points[:, 0] > -1.0) & (points[:, 0] < 1.0)
-        pass_y = (points[:, 1] > -1.0) & (points[:, 1] < 1.0)
-        filtered_indices = np.where(pass_x & pass_y)[0]
+                a, b, c, d = plane_model
+                normal = np.array([a, b, c])
 
-        # フィルタリング後の点群を更新
-        filtered_pcd = pcd.select_by_index(filtered_indices)
-        filtered_pcd = filtered_pcd.voxel_down_sample(voxel_size=0.005)  # 点群をダウンサンプリング
+                if np.abs(normal[1]) < 0.9:
+                    inlier_cloud.paint_uniform_color([1.0, 0, 0])
+                    angle_with_vertical = np.arccos(np.abs(normal[1]))
+                    angle_degrees = np.degrees(angle_with_vertical)
+                    rospy.loginfo(f"Ground plane tilt angle: {angle_degrees:.2f} degrees")
 
-        # 地面の平面を検出
-        plane_model, inliers = filtered_pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=100)
-        inlier_cloud = filtered_pcd.select_by_index(inliers)
+                    plane_segments = [inlier_cloud]
+                else:
+                    plane_segments = []
 
-        # 法線が特定の閾値を持つ場合に平面として認識
-        a, b, c, d = plane_model
-        normal = np.array([a, b, c])
-        
-        # 地面の傾きを計算
-        if np.abs(normal[1]) < 0.9:  # 法線が水平すぎない場合
-            inlier_cloud.paint_uniform_color([1.0, 0, 0])
+                if plane_segments:
+                    combined_points = np.vstack([np.asarray(plane.points) for plane in plane_segments])
+                    combined_colors = np.vstack([np.asarray(plane.colors) for plane in plane_segments])
 
-            # 傾きを計算
-            angle_with_vertical = np.arccos(np.abs(normal[1]))  # y軸との角度
-            angle_degrees = np.degrees(angle_with_vertical)
-            rospy.loginfo(f"Ground plane tilt angle: {angle_degrees:.2f} degrees")
+                    self.pointcloud.points = o3d.utility.Vector3dVector(combined_points)
+                    self.pointcloud.colors = o3d.utility.Vector3dVector(combined_colors)
 
-            plane_segments = [inlier_cloud]
-        else:
-            plane_segments = []
+                    if not self.geom_added:
+                        self.vis.add_geometry(self.pointcloud)
+                        self.geom_added = True
+                    else:
+                        self.vis.update_geometry(self.pointcloud)
 
-        # 点群を更新
-        if plane_segments:  # plane_segmentsが空でない場合のみ処理
-            combined_points = np.vstack([np.asarray(plane.points) for plane in plane_segments])
-            combined_colors = np.vstack([np.asarray(plane.colors) for plane in plane_segments])
+                self.vis.poll_events()
+                self.vis.update_renderer()
+                cv2.imshow('bgr', self.color_image)
+                key = cv2.waitKey(1)
+                if key == ord('q'):
+                    break
+                self.rate.sleep()
 
-            pointcloud.points = o3d.utility.Vector3dVector(combined_points)
-            pointcloud.colors = o3d.utility.Vector3dVector(combined_colors)
+        finally:
+            o3d.io.write_point_cloud("output2.ply", self.pointcloud)
+            cv2.destroyAllWindows()
+            self.vis.destroy_window()
 
-            if not geom_added:
-                vis.add_geometry(pointcloud)
-                geom_added = True
-            else:
-                vis.update_geometry(pointcloud)
-
-        vis.poll_events()
-        vis.update_renderer()
-        cv2.imshow('bgr', color_image)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-        rate.sleep()
-
-finally:
-    o3d.io.write_point_cloud("output2.ply", pointcloud)
-    cv2.destroyAllWindows()
-    vis.destroy_window()
-
-
-
-"""
-# 地面一つだけ
-import rospy
-from sensor_msgs.msg import Image, CameraInfo
-from cv_bridge import CvBridge
-import numpy as np
-import open3d as o3d
-import cv2
-
-# グローバル変数の定義
-color_image = None
-depth_image = None
-intrinsics = None
-
-# コールバック関数
-def color_callback(msg):
-    global color_image
-    color_image = bridge.imgmsg_to_cv2(msg, "bgr8")
-
-def depth_callback(msg):
-    global depth_image
-    depth_image = bridge.imgmsg_to_cv2(msg, "16UC1")
-
-def camera_info_callback(msg):
-    global intrinsics
-    intrinsics = msg
-
-# ROSノードの初期化
-rospy.init_node('pointcloud_node')
-bridge = CvBridge()
-
-# トピックの購読
-rospy.Subscriber('/camera/color/image_raw', Image, color_callback)
-rospy.Subscriber('/camera/aligned_depth_to_color/image_raw', Image, depth_callback)
-rospy.Subscriber('/camera/aligned_depth_to_color/camera_info', CameraInfo, camera_info_callback)
-
-# Open3Dの設定
-vis = o3d.visualization.Visualizer()
-vis.create_window('PCD', width=640, height=480)
-pointcloud = o3d.geometry.PointCloud()
-geom_added = False
-
-# ループ内の処理を制限
-rate = rospy.Rate(10)  # 10Hzに設定
-
-try:
-    while not rospy.is_shutdown():
-        if color_image is None or depth_image is None or intrinsics is None:
-            rospy.loginfo("Waiting for images and camera info...")
-            rospy.sleep(0.1)
-            continue
-
-        # 画像変換を事前に行う
-        o3d_color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
-        img_depth = o3d.geometry.Image(depth_image)
-        img_color = o3d.geometry.Image(o3d_color_image)
-        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(img_color, img_depth, convert_rgb_to_intensity=False)
-
-        pinhole_camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(
-            intrinsics.width, intrinsics.height, intrinsics.K[0], intrinsics.K[4], intrinsics.K[2], intrinsics.K[5])
-
-        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd, pinhole_camera_intrinsic)
-        pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-
-        # フィルタリングのインデックスを毎回計算
-        points = np.asarray(pcd.points)
-        pass_x = (points[:, 0] > -1.0) & (points[:, 0] < 1.0)
-        pass_y = (points[:, 1] > -1.0) & (points[:, 1] < 1.0)
-        filtered_indices = np.where(pass_x & pass_y)[0]
-
-        # フィルタリング後の点群を更新
-        filtered_pcd = pcd.select_by_index(filtered_indices)
-        filtered_pcd = filtered_pcd.voxel_down_sample(voxel_size=0.005)  # 点群をダウンサンプリング
-
-        # 平面検出の実行
-        if len(filtered_pcd.points) > 0:
-            plane_model, inliers = filtered_pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=100)
-            # 平面モデルの係数を出力
-            [a, b, c, d] = plane_model
-            inlier_cloud = filtered_pcd.select_by_index(inliers)
-            # インライアの点を抽出して色を付ける
-            inlier_cloud.paint_uniform_color([1.0, 0, 0])
-            # 平面以外の点を抽出
-            outlier_cloud = filtered_pcd.select_by_index(inliers, invert=True)
-
-            # インライアとアウトライアの点群を結合
-            combined_points = np.vstack((np.asarray(inlier_cloud.points), np.asarray(outlier_cloud.points)))
-            combined_colors = np.vstack((np.asarray(inlier_cloud.colors), np.asarray(outlier_cloud.colors)))
-
-            # 点群を更新
-            pointcloud.points = o3d.utility.Vector3dVector(combined_points)
-            pointcloud.colors = o3d.utility.Vector3dVector(combined_colors)
-
-            if not geom_added:
-                vis.add_geometry(pointcloud)
-                geom_added = True
-            else:
-                vis.update_geometry(pointcloud)
-
-            vis.poll_events()
-            vis.update_renderer()
-
-        rate.sleep()
-
-finally:
-    o3d.io.write_point_cloud("output2.ply", pointcloud)
-    cv2.destroyAllWindows()
-    vis.destroy_window()
-"""
+if __name__ == '__main__':
+    processor = PointCloudProcessor()
+    processor.process_pointcloud()
--- slope_edge_detection/scripts/3dd/ransac_slope.py
+++ slope_edge_detection/scripts/3dd/ransac_slope.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
-import sys
-import cv2
-import numpy as np
-import csv
 import rospy
-from datetime import datetime
 from sensor_msgs.msg import Image, CameraInfo
-from geometry_msgs.msg import TransformStamped
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+import open3d as o3d
+import cv2
 from message_filters import Subscriber, ApproximateTimeSynchronizer
 from tf import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
 from ultralytics import YOLO
+from datetime import datetime
+#import csv
 from PIL import Image as PilImage
-import open3d as o3d
+import sys
+
+# グローバル変数の定義
+img_color = None
+img_depth = None
+intrinsics = None
 
 class SlopeDetection:
 
@@ -21,38 +26,29 @@ class SlopeDetection:
         self.bridge = CvBridge()
         self.frame_id = 'slope'
         
-        self.sub_info = Subscriber('camera/aligned_depth_to_color/camera_info', CameraInfo)
-        self.sub_color = Subscriber('camera/color/image_raw', Image)
-        self.sub_depth = Subscriber('camera/aligned_depth_to_color/image_raw', Image)
+        # トピックの購読と同期
+        self.sub_info = Subscriber('/camera/aligned_depth_to_color/camera_info', CameraInfo)
+        self.sub_color = Subscriber('/camera/color/image_raw', Image)
+        self.sub_depth = Subscriber('/camera/aligned_depth_to_color/image_raw', Image)
         
-        # 画像とカメラ情報のトピックを同期させる
         self.ts = ApproximateTimeSynchronizer([self.sub_info, self.sub_color, self.sub_depth], 20, 0.1)
         self.ts.registerCallback(self.images_callback)
         
-        # Transform broadcaster
         self.broadcaster = TransformBroadcaster()
         
         # CSV file
-        current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
-        file_name = f'slope_3次元座標{current_time}.csv'
-        self.csv_file = open(file_name, 'w', newline='')
-        self.csv_writer = csv.writer(self.csv_file)
-        self.csv_writer.writerow(['name', 'X', 'Y', 'Z', 'median_x', 'median_y'])
-
-        # Open3Dの設定
-        vis = o3d.visualization.Visualizer()
-        vis.create_window('PCD', width=640, height=480)
-        pointcloud = o3d.geometry.PointCloud()
-        geom_added = False
-
-        # ループ内の処理を制限
-        rate = rospy.Rate(10)  # 10Hzに設定
+        # current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
+        # file_name = f'slope_3次元座標{current_time}.csv'
+        # self.csv_file = open(file_name, 'w', newline='')
+        # self.csv_writer = csv.writer(self.csv_file)
+        # self.csv_writer.writerow(['name', 'X', 'Y', 'Z', 'median_x', 'median_y'])
 
     def images_callback(self, msg_info, msg_color, msg_depth):
+        global img_color, img_depth, intrinsics
         try:
-            img_color = self.bridge.imgmsg_to_cv2(msg_color, 'bgr8')
-            img_depth = self.bridge.imgmsg_to_cv2(msg_depth, 'passthrough').copy()
-            
+            img_color = self.bridge.imgmsg_to_cv2(msg_color, "bgr8")
+            img_depth = self.bridge.imgmsg_to_cv2(msg_depth, "16UC1")
+            intrinsics = msg_info
         except CvBridgeError as e:
             rospy.logwarn(str(e))
             return
@@ -62,24 +58,14 @@ class SlopeDetection:
         if img_color.shape[0:2] != img_depth.shape[0:2]:
             rospy.logwarn('カラーと深度の画像サイズが異なる')
             return
-            
-        # Use YOLO model to detect slope
-        pil_img = PilImage.fromarray(cv2.cvtColor(img_color, cv2.COLOR_BGR2RGB))
-        results = self.model.predict(source=pil_img)
-
-        # 予測結果がない場合の処理
-        if not results or not results[0].masks:
-            rospy.logwarn("予測結果が存在しません")
-            return
-        
-        # 信頼度の取得と表示
-        mask_confidences = results[0].boxes.conf
-        print("信頼度:", mask_confidences)
-        
-         # 画像変換を事前に行う
-        o3d_color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
-        img_depth = o3d.geometry.Image(depth_image)
-        img_color = o3d.geometry.Image(o3d_color_image)
+        if img_color is not None:
+            print(f"img_color type: {type(img_color)}, shape: {img_color.shape}")
+        else:
+            print("img_color is None")
+        # 画像変換を事前に行う
+        o3d_img_color = cv2.cvtColor(img_color, cv2.COLOR_BGR2RGB)
+        img_depth = o3d.geometry.Image(img_depth)
+        img_color = o3d.geometry.Image(o3d_img_color)
         rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(img_color, img_depth, convert_rgb_to_intensity=False)
 
         pinhole_camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(
@@ -98,22 +84,32 @@ class SlopeDetection:
         filtered_pcd = pcd.select_by_index(filtered_indices)
         filtered_pcd = filtered_pcd.voxel_down_sample(voxel_size=0.005)  # 点群をダウンサンプリング
 
-        # 平面検出の実行
-        if len(filtered_pcd.points) > 0:
-            plane_model, inliers = filtered_pcd.segment_plane(distance_threshold=0.01, ransac_n=30, num_iterations=100)
-            # 平面モデルの係数を出力
-            [a, b, c, d] = plane_model
-            inlier_cloud = filtered_pcd.select_by_index(inliers)
-            # インライアの点を抽出して色を付ける
+        # 地面の平面を検出
+        plane_model, inliers = filtered_pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=100)
+        inlier_cloud = filtered_pcd.select_by_index(inliers)
+
+        # 法線が特定の閾値を持つ場合に平面として認識
+        a, b, c, d = plane_model
+        normal = np.array([a, b, c])
+        
+        # 地面の傾きを計算
+        if np.abs(normal[1]) < 0.9:  # 法線が水平すぎない場合
             inlier_cloud.paint_uniform_color([1.0, 0, 0])
-            # 平面以外の点を抽出
-            outlier_cloud = filtered_pcd.select_by_index(inliers, invert=True)
 
-            # インライアとアウトライアの点群を結合
-            combined_points = np.vstack((np.asarray(inlier_cloud.points), np.asarray(outlier_cloud.points)))
-            combined_colors = np.vstack((np.asarray(inlier_cloud.colors), np.asarray(outlier_cloud.colors)))
+            # 傾きを計算
+            angle_with_vertical = np.arccos(np.abs(normal[1]))  # y軸との角度
+            angle_degrees = np.degrees(angle_with_vertical)
+            rospy.loginfo(f"Ground plane tilt angle: {angle_degrees:.2f} degrees")
 
-            # 点群を更新
+            plane_segments = [inlier_cloud]
+        else:
+            plane_segments = []
+
+        # 点群を更新
+        if plane_segments:  # plane_segmentsが空でない場合のみ処理
+            combined_points = np.vstack([np.asarray(plane.points) for plane in plane_segments])
+            combined_colors = np.vstack([np.asarray(plane.colors) for plane in plane_segments])
+
             pointcloud.points = o3d.utility.Vector3dVector(combined_points)
             pointcloud.colors = o3d.utility.Vector3dVector(combined_colors)
 
@@ -123,12 +119,19 @@ class SlopeDetection:
             else:
                 vis.update_geometry(pointcloud)
 
-            vis.poll_events()
-            vis.update_renderer()
-
-        rate.sleep()
-        # 信頼度が90%以上か確認
-        if results[0].boxes and results[0].boxes.conf[0] > 0.8
+        # 信頼度の取得と表示
+        mask_confidences = results[0].boxes.conf
+        print("信頼度:", mask_confidences)
+           
+        if angle_degrees >= 40.0 and results and results[0].boxes and results[0].boxes.conf[0] > 0.8:
+            # YOLOによるスロープ検出
+            pil_img = PilImage.fromarray(cv2.cvtColor(img_color, cv2.COLOR_BGR2RGB))
+            results = self.model.predict(source=pil_img)
+            # 予測結果がない場合の処理
+            if not results or not results[0].masks:
+                rospy.logwarn("予測結果が存在しません")
+                return
+                
             masks = results[0].masks
             x_numpy = masks[0].data.to('cpu').detach().numpy().copy()
 
@@ -176,41 +179,7 @@ class SlopeDetection:
             median_y = int(np.median([p[1] for p in top_points_y_sorted]))
 
             # 中央の点を描画
-            cv2.circle(img_color, (median_x, median_y), 10, (255, 0, 0), -1)
-            
-            """
-            #左により杉
-            # y座標のピクセル値が高い順に上位50個を選ぶ
-            top_points_y_sorted = sorted(top_points, key=lambda p: p[1], reverse=True)[:100]
-
-            # x座標の中央値を計算するために、上位50個のうちx座標の範囲を絞る
-            median_x_candidates = [p for p in top_points if p[0] < np.median([p[0] for p in top_points])]
-
-            # y座標が高い2つの点を選ぶ
-            (u1, v1) = (int(top_points_y_sorted[0][0]), int(top_points_y_sorted[0][1]))
-            (u2, v2) = (int(top_points_y_sorted[1][0]), int(top_points_y_sorted[1][1]))
-
-            # x座標とy座標の中央値をそれぞれ算出
-            median_x = int(np.median([p[0] for p in median_x_candidates]))
-            median_y = int(np.median([p[1] for p in top_points_y_sorted]))
-
-            # 中央の点を描画
-            cv2.circle(img_color, (median_x, median_y), 10, (255, 0, 0), -1)  
-            """  
-            
-            """
-            #元のやつ
-            # 上位50個の座標の中から最もy座標が高い2つの点を選びu1u2などをそれに当てる
-            top_points_sorted = sorted(top_points, key=lambda p: p[1], reverse=True)
-            (u1, v1) = (int(top_points[0][0]), int(top_points[0][1]))
-            (u2, v2) = (int(top_points[1][0]), int(top_points[1][1]))
-
-            # 上位50個の座標の中央値を算出
-            median_x = int(np.median([p[0] for p in top_points_sorted]))
-            median_y = int(np.median([p[1] for p in top_points_sorted]))
-            cv2.circle(img_color, (median_x, median_y), 10, (255, 0, 0), -1)
-            """
-                 
+            cv2.circle(img_color, (median_x, median_y), 10, (255, 0, 0), -1)                 
                         
             #映像出力rosbag playでやるときのみ外す
             # cv2.imshow('color', img_color)
@@ -218,7 +187,7 @@ class SlopeDetection:
             #     rospy.signal_shutdown('closed')
             #     return
             # Write to CSV
-            self.csv_writer.writerow([self.frame_id, x, y, z, median_x, median_y])
+            # self.csv_writer.writerow([self.frame_id, x, y, z, median_x, median_y])
 
             # Broadcast transform
             ts = TransformStamped()
@@ -229,17 +198,41 @@ class SlopeDetection:
             ts.transform.translation.z = z
             self.broadcaster.sendTransform((x, y, z), (0, 0, 0, 1), rospy.Time.now(), self.frame_id, msg_depth.header.frame_id)
 
-    def __del__(self):
-        self.csv_file.close()
-        
+    # def __del__(self):
+    #     self.csv_file.close()
+
 if __name__ == '__main__':
     rospy.init_node('slope_detection')
     model_path = "/home/carsim/gakuhari_ws/src/ros_Golib/slope_edge_detection/scripts/best.pt"
     node = SlopeDetection(model_path)
+    vis = o3d.visualization.Visualizer()
+    vis.create_window('PCD', width=960, height=720)
+    pointcloud = o3d.geometry.PointCloud()
+    geom_added = False
+    rate = rospy.Rate(10)  # 10Hzに設定
+    
     try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        if node.csv_file:
-            node.csv_file.close()
-        pass
-    rospy.signal_shutdown('KeyboardInterrupt')
+        while not rospy.is_shutdown():
+            if img_color is None or img_depth is None or intrinsics is None:
+                rospy.loginfo("Waiting for images and camera info...")
+                rospy.sleep(0.1)
+                continue
+
+            vis.poll_events()
+            vis.update_renderer()
+            
+            # ここで型を確認
+            if isinstance(img_color, np.ndarray):
+                cv2.imshow('bgr', img_color)
+            else:
+                rospy.logwarn("img_color is not a valid numpy array")
+
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+            rate.sleep()
+
+    finally:
+        o3d.io.write_point_cloud("output2.ply", pointcloud)
+        cv2.destroyAllWindows()
+        vis.destroy_window()
